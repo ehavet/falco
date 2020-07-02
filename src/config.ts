e7c2cc628a633345fff3882bb28853d2b0d29608
Mathieu Laurent <mlaurent@octo.com>
2020-07-02 10:47:24 +0200

    Add dotenv config

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 import { Envie, Joi } from 'envie'
+require('dotenv').config()
 
 export type Config = Map<string, any>
 
 module.exports = Envie({
 
-  APP_NAME: Joi.string().default('falco-api'),
+  APP_NAME: Joi.string(),
 
-  DATABASE_URL: Joi.string().uri({ scheme: ['postgres'] }).default('postgres://test:test@localhost:54334/test')
-    .description('Connection string of the main database'),
+  DATABASE_URL: Joi.string().uri({ scheme: ['postgres'] }).description('Connection string of the main database'),
 
-  LOG_LEVEL: Joi.string().valid('fatal', 'error', 'warn', 'info', 'debug', 'trace').default('debug'),
+  LOG_LEVEL: Joi.string().valid('fatal', 'error', 'warn', 'info', 'debug', 'trace'),
 
-  PORT: Joi.number().min(0).default(8080)
+  PORT: Joi.number().min(0)
     .description('Port on which the HTTP server will listen'),
 
-  URL_PREFIX: Joi.string().default('/')
+  URL_PREFIX: Joi.string()
 
 }) as Config

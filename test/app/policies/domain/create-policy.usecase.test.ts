2b4fa35d75b5461f39b5e6ffe7ad6ff2993ef673
Mathieu Laurent <mlaurent@octo.com>
2020-07-02 21:57:47 +0200

    Create Policy Usecase test small refacto

--- test/app/policies/domain/create-policy.usecase.test.ts
+++ test/app/policies/domain/create-policy.usecase.test.ts
@@ -1,36 +1,19 @@
 import { Policy } from '../../../../src/app/policies/domain/policy'
 import { CreatePolicyQuery } from '../../../../src/app/policies/domain/create-policy-query'
 import { expect, sinon } from '../../../test-utils'
-import { CreatePolicy } from '../../../../src/app/policies/domain/create-policy'
+import { CreatePolicy } from '../../../../src/app/policies/domain/create-policy.usecase'
 import { Quote } from '../../../../src/app/quotes/domain/quote'
 import { QuoteRepository } from '../../../../src/app/quotes/domain/quote.repository'
 import { SinonStubbedInstance } from 'sinon'
+import { createQuote } from '../../quotes/fixtures/quote.fixture'
 
 describe('Policies - Usecase - Create policy', async () => {
   describe('should return the newly created policy', async () => {
-    const quote: Quote = {
-      id: 'J4F56T4',
-      partnerCode: 'myPartner',
-      risk: {
-        property: {
-          roomCount: 2
-        }
-      },
-      insurance: {
-        estimate: {
-          monthlyPrice: 5.82,
-          defaultDeductible: 150,
-          defaultCeiling: 7000
-        },
-        currency: 'EUR',
-        simplifiedCovers: ['ACDDE', 'ACVOL'],
-        productCode: 'MRH_Etudiant',
-        productVersion: '1.0'
-      }
-    }
+    const quote: Quote = createQuote()
+
     it('with the insurance', async () => {
       // Given
-      const createPolicyQuery: CreatePolicyQuery = { quoteId: 'J4F56T4' }
+      const createPolicyQuery: CreatePolicyQuery = { quoteId: quote.id }
       const quoteRepository: SinonStubbedInstance<QuoteRepository> = { save: sinon.stub(), get: sinon.stub() }
       const createPolicy: CreatePolicy = CreatePolicy.factory(quoteRepository)
       const expectedPolicy: Policy = {
@@ -42,8 +25,8 @@ describe('Policies - Usecase - Create policy', async () => {
           },
           currency: 'EUR',
           simplifiedCovers: ['ACDDE', 'ACVOL'],
-          productCode: 'MRH_Etudiant',
-          productVersion: '1.0'
+          productCode: 'MRH-Loc-Etud',
+          productVersion: 'v2020-02-01'
         }
       }
 

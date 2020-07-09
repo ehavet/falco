ab92c1e810a2623d8028ff6c862518aac4bb38a6
Mathieu Laurent <mlaurent@octo.com>
2020-07-09 18:33:07 +0200

    Email Validation fix e2e test

--- test/app/email-validations/api/v0/email-validations.api.e2e-test.ts
+++ test/app/email-validations/api/v0/email-validations.api.e2e-test.ts
@@ -1,5 +1,14 @@
 import * as supertest from 'supertest'
-import { expect, HttpServerForTesting, newProdLikeServer } from '../../../../test-utils'
+import { dateFaker, expect, HttpServerForTesting, newProdLikeServer } from '../../../../test-utils'
+import { PolicySqlModel } from '../../../../../src/app/policies/infrastructure/policy-sql.model'
+import { Policy } from '../../../../../src/app/policies/domain/policy'
+import { PolicyRepository } from '../../../../../src/app/policies/domain/policy.repository'
+import { PolicySqlRepository } from '../../../../../src/app/policies/infrastructure/policy-sql.repository'
+import { createPolicyFixture } from '../../../policies/fixtures/policy.fixture'
+
+async function resetDb () {
+  await PolicySqlModel.destroy({ truncate: true, cascade: true })
+}
 
 describe('Email Validations - API - E2E', async () => {
   let httpServer: HttpServerForTesting
@@ -9,20 +18,38 @@ describe('Email Validations - API - E2E', async () => {
 
   describe('POST /internal/v0/email-validation/validate', async () => {
     let response: supertest.Response
+    const now: Date = new Date('2020-08-12T00:00:00.000Z')
+    const policyRepository: PolicyRepository = new PolicySqlRepository()
 
-    it('should return a callback URI', async () => {
+    before(async () => {
       // GIVEN
+      dateFaker.setCurrentDate(now)
+      const policy: Policy = createPolicyFixture({ id: 'APP746312047', emailValidationDate: undefined })
+      await policyRepository.save(policy)
       const validUnexpiredToken: string =
-          'RibdLLppjhpKAqrcTsT1KxQWH70WcFoOPIuSvHJXdtzDQUDmZxk1SnDNmXFIfcJLBhlVCk8+uJoDVUJreeHEhIoQ2Bc4ARbFWnADoJ1AEM/vgfhZwchBJRqI16CrPa39vqyWPfK0cFIYBgV313oVhg=='
+          'LvzG5tpyGSc8m/hvZFlClWpBm8fsR8VZxZcxZOXcWSZm5z25OT1PoUKkXncKSJ58z4YnzvIyPPCJXf4hDaAl7sewepq1p2jENhd/Y9A6Hft7NJPaldA6w2Kgyz3JvicXqysO7t4RhC4GHUwnGto1Vtw/eJGdBrEE2Brg9Sdo1bMCXZsLMsIP+0GwQba41UpV'
+
       // WHEN
       response = await httpServer.api()
         .post('/internal/v0/email-validations/validate')
         .send({
           token: validUnexpiredToken
         })
+    })
 
+    after(async () => {
+      await resetDb()
+    })
+
+    it('should return a callback URI', async () => {
       // THEN
-      expect(response.body).to.deep.equal({ callback_url: 'http://callback.url.com' })
+      expect(response.body).to.deep.equal({ callback_url: 'http://bicycle-day.com' })
+    })
+
+    it('should update the policy email validation date', async () => {
+      // THEN
+      const updatedPolicy: Policy = await policyRepository.get('APP746312047')
+      expect(updatedPolicy.emailValidationDate).to.deep.equal(now)
     })
   })
 })

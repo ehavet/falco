4a3de4203645b8a0b1247c89ed3511fa9d07c2d8
Mathieu Laurent <mlaurent@octo.com>
2020-07-17 12:20:37 +0200

    Generate contract to sign

---
+++ src/app/policies/domain/contract/contract.ts
@@ -0,0 +1,4 @@
+export interface Contract {
+    name: string,
+    buffer: Buffer
+}
---
+++ test/app/policies/infrastructure/contract-pdf.generator.test.ts
@@ -0,0 +1,28 @@
+import fsx from 'fs-extra'
+import { createPolicyFixture } from '../fixtures/policy.fixture'
+import { config, expect } from '../../../test-utils'
+import { ContractPdfGenerator } from '../../../../src/app/policies/infrastructure/contract/contract-pdf.generator'
+import { SpecificTermsGenerator } from '../../../../src/app/policies/domain/specific-terms/specific-terms.generator'
+import { SpecificTermsPdfGenerator } from '../../../../src/app/policies/infrastructure/specific-terms-pdf/specific-terms-pdf.generator'
+
+describe('Policies - Infra - Contract PDF Generator', async () => {
+  const specificTermsFolderPath: string = config.get('FALCO_API_DOCUMENTS_STORAGE_FOLDER')
+  const specificTermsGenerator: SpecificTermsGenerator = new SpecificTermsPdfGenerator()
+  const subscriptionDocumentsGenerator: ContractPdfGenerator = new ContractPdfGenerator()
+
+  it('should generate a new contract with signature page, specific terms and contractual terms', async () => {
+    // Given
+    await fsx.emptyDir(specificTermsFolderPath)
+    const policy = createPolicyFixture({ partnerCode: 'essca' })
+    const specificTerms = await specificTermsGenerator.generate(policy)
+
+    // When
+    const subscriptionDocuments = await subscriptionDocumentsGenerator.generate(policy.id, specificTerms)
+
+    // Then
+    expect(subscriptionDocuments.name).to.equal('Appenin_Contrat_assurance_habitation_APP753210859.pdf')
+    expect(subscriptionDocuments.buffer.includes('Par cette signature, j\\222accepte les termes du contrat')).to.be.true
+    expect(subscriptionDocuments.buffer.includes('(P)87.9 (AR)49.3 (TICULI\\310RES)')).to.be.true
+    expect(subscriptionDocuments.buffer.includes('CONDITIONS G\\311N\\311RALES')).to.be.true
+  })
+})

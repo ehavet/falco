15fd32f880552e91b905eb8a7e3ff032a00b9090
Ruben <ruben.desert@appenin.fr>
2020-12-17 00:59:46 +0100

    fix: Add generated hash for E2E testing on signature processor

--- test/configs/hellosign.test.config.ts
+++ test/configs/hellosign.test.config.ts
@@ -1,14 +1,8 @@
+import { HelloSignConfig } from '../../src/configs/hello-sign.config'
+import { config } from '../test-utils'
 const hellosignSdk = require('hellosign-sdk')
-const config = require('../config')
 
-export interface HelloSignConfig {
-    hellosign
-    clientId: string
-    testMode: boolean
-    key: string
-}
-
-export const helloSignConfig: HelloSignConfig = {
+export const helloSignConfigTest: HelloSignConfig = {
   hellosign: hellosignSdk({ key: config.get('FALCO_API_HELLO_SIGN_API_KEY') }),
   clientId: config.get('FALCO_API_HELLO_SIGN_CLIENT_ID'),
   testMode: !config.get('FALCO_API_HELLO_SIGN_PRODUCTION_MODE'),

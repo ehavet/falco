65a951c685b6ffc41f72f1a498a7bcfa134c36a8
Mathieu Laurent <mlaurent@octo.com>
2021-02-02 18:34:38 +0100

    refacto(quotes): refacto create and update quotes domain to harmonize

--- src/app/quotes/domain/create-quote-command.ts
+++ src/app/quotes/domain/create-quote-command.ts
@@ -4,30 +4,42 @@ import { Occupancy } from '../../common-api/domain/type/occupancy'
 
 export interface CreateQuoteCommand {
     partnerCode: string,
-    specOpsCode: OperationCode
-    risk: {
-        property: {
-            roomCount: number,
-            address?: string,
-            postalCode?: string,
-            city?: string,
-            type?: PropertyType,
-            occupancy?: Occupancy
-        },
-        person?: {
-            firstname: string,
-            lastname: string
-        },
-        otherPeople?: Array<{ firstname: string, lastname: string }>
-    },
-    policyHolder?: {
+    risk: CreateQuoteCommand.Risk,
+    policyHolder?: CreateQuoteCommand.PolicyHolder,
+    startDate?: Date,
+    specOpsCode?: OperationCode
+}
+
+export namespace CreateQuoteCommand {
+    export interface Risk {
+        property: Risk.Property,
+        person?: Risk.Person
+        otherPeople?: Array<Risk.Person>
+    }
+
+    export interface PolicyHolder {
         firstname?: string,
         lastname?: string,
         address?: string,
         postalCode?: string,
         city?: string,
         email?: string,
-        phoneNumber?: string,
-    },
-    startDate?: Date
+        phoneNumber?: string
+    }
+}
+
+export namespace CreateQuoteCommand.Risk {
+    export interface Property {
+        roomCount: number,
+        address?: string,
+        postalCode?: string,
+        city?: string
+        type?: PropertyType,
+        occupancy?: Occupancy
+    }
+
+    export interface Person {
+        firstname: string,
+        lastname: string
+    }
 }

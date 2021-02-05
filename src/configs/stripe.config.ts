cf4a87ad236c446d2482423ebb2e7da9c1ca5340
appvinc <vincent.bataille+github@appenin.fr>
2021-02-05 10:40:18 +0100

    fix: properly check stripe test webhook signature

--- src/configs/stripe.config.ts
+++ src/configs/stripe.config.ts
@@ -4,9 +4,11 @@ const config = require('../config')
 export interface StripeConfig {
     stripe: StripeClients
     eventHandlerSecret: string
+    eventHandlerSecretTest: string
 }
 
 export const stripeConfig = {
   stripe,
-  eventHandlerSecret: config.get('FALCO_API_STRIPE_WEBHOOK_SECRET')
+  eventHandlerSecret: config.get('FALCO_API_STRIPE_WEBHOOK_SECRET'),
+  eventHandlerSecretTest: config.get('FALCO_API_STRIPE_WEBHOOK_SECRET_TEST')
 }
